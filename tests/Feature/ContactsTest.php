25a690059cd72121b562c0c600a1253c977a404d
Angel Mendoza <1930.kick@gmail.com>
2019-08-10 01:32:59 -0500

    add: Api token authentication

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Contact;
+use App\User;
 use Carbon\Carbon;
 use function foo\func;
 use Tests\TestCase;
@@ -13,10 +14,29 @@ class ContactsTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
-    public function a_contact_can_be_added()
+    protected $user;
+
+    protected function setUp(): void
     {
-        $this->post('/api/contacts', $this->data());
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /** @test */
+    public function an_unauthenticated_user_should_be_redirected_to_login()
+    {
+        $response = $this->post('/api/contacts', $this->data());
+
+        $response->assertRedirect('/login');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_add_a_contact()
+    {
+        // $this->post('/api/contacts', array_merge($this->data(), ['api_token' => $user->api_token]));
+        $this->post('/api/contacts', $this->data(), $this->headers());
 
         $contact = Contact::first();
 
@@ -32,7 +52,7 @@ class ContactsTest extends TestCase
         collect(['name', 'email', 'birthday', 'company'])
             ->each(function ($field) {
                 $response = $this->post('/api/contacts',
-                    array_merge($this->data(), [$field => '']));
+                    array_merge($this->data(), [$field => '']), $this->headers());
 
                 $response->assertSessionHasErrors($field);
                 $this->assertCount(0, Contact::all());
@@ -43,7 +63,7 @@ class ContactsTest extends TestCase
     public function email_must_be_a_valid_email()
     {
         $response = $this->post('/api/contacts',
-            array_merge($this->data(), ['email' => 'NOT AN EMAIL']));
+            array_merge($this->data(), ['email' => 'NOT AN EMAIL']), $this->headers());
 
         $response->assertSessionHasErrors('email');
         $this->assertCount(0, Contact::all());
@@ -53,7 +73,7 @@ class ContactsTest extends TestCase
     public function birthdays_are_properly_stored()
     {
         $this->withoutExceptionHandling();
-        $response = $this->post('/api/contacts', $this->data());
+        $response = $this->post('/api/contacts', $this->data(), $this->headers());
 
         $this->assertCount(1, Contact::all());
         $this->assertInstanceOf(Carbon::class, Contact::first()->birthday);
@@ -64,7 +84,7 @@ class ContactsTest extends TestCase
     public function a_contact_can_be_retrieved()
     {
         $contact = factory(Contact::class)->create();
-        $response = $this->get('/api/contacts/'. $contact->id);
+        $response = $this->get('/api/contacts/' . $contact->id, $this->headers());
 
         $response->assertJson([
             'name' => $contact->name,
@@ -78,7 +98,7 @@ class ContactsTest extends TestCase
     public function a_contact_can_be_patched()
     {
         $contact = factory(Contact::class)->create();
-        $response = $this->patch('/api/contacts/'.$contact->id, $this->data());
+        $response = $this->patch('/api/contacts/' . $contact->id, $this->data(), $this->headers());
 
         $contact = $contact->fresh();
 
@@ -92,11 +112,11 @@ class ContactsTest extends TestCase
     public function a_contact_can_be_deleted()
     {
         $contact = factory(Contact::class)->create();
-        $response = $this->delete('/api/contacts/'.$contact->id);
+        $response = $this->delete('/api/contacts/' . $contact->id, [], $this->headers());
 
         $this->assertCount(0, Contact::all());
     }
-    
+
     private function data()
     {
         return [
@@ -104,6 +124,13 @@ class ContactsTest extends TestCase
             'email' => 'test@gmail.com',
             'birthday' => '05/14/1988',
             'company' => 'ABC Company'
+        ];
+    }
+
+    private function headers()
+    {
+        return [
+            'Authorization' => 'Bearer ' . $this->user->api_token,
         ];
     }
 }
