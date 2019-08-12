e766c850cd8ca0a6234bc6fe872ae3be50a0ee12
Angel Mendoza <1930.kick@gmail.com>
2019-08-12 12:32:05 -0500

    add: Create Contact Form

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ExampleComponent from "./components/ExampleComponent";
-
+import ContactsCreate from './views/ContactsCreate'
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            component: ExampleComponent,
-        }
-    ]
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: ExampleComponent,
+    },
+    {
+      path: '/contacts/create',
+      component: ContactsCreate,
+    },
+  ]
 })
---
+++ resources/js/views/ContactsCreate.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <InputField name="name" label="Nombre del Contacto" placeholder="Nombre del Contacto" @update:field="form.name = $event"/>
+      <InputField name="email" label="Correo del Contacto" placeholder="Correo del Contacto" @update:field="form.email = $event"/>
+      <InputField name="company" label="Nombre de la Compañia" placeholder="Nombre de la Compañia" @update:field="form.company = $event"/>
+      <InputField name="birthday" label="Fecha de Nacimiento" placeholder="MM/DD/YYYY" @update:field="form.birthday = $event"/>
+
+      <div class="flex justify-end">
+        <button class="py-2 px-4 rounded text-red-700 border mr-5 hover:border-red-700">Cancelar</button>
+        <button type="submit" class="bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-400">Agregar al Nuevo Contacto</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import InputField from '../components/InputField'
+  export default {
+    name: 'ContactsCreate',
+    components: {
+      InputField
+    },
+    data() {
+      return {
+        form: {
+          'name': '',
+          'email': '',
+          'company': '',
+          'birthday': '',
+        }
+      }
+    },
+    methods: {
+      async submitForm() {
+        try {
+          const res = await axios.post('/api/contacts', this.form)
+          console.log(res)
+        }catch (err) {
+          console.log(err)
+        }
+      }
+    }
+  }
+</script>
